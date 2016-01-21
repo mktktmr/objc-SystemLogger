c315b4641269f53244c555c30cd19e7bcc63a4dc
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-01-21 15:02:51 +0900

    add demo project

---
+++ demo/DemoSystemLogger/DemoSystemLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoSystemLogger
+//
+//  Created by Makoto Kitamura on 2016/01/21.
+//  Copyright © 2016年 Makoto Kitamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
